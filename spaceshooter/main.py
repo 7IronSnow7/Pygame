05fc94aaa74f84b82a569d8dd0513308ba463403
7IronSnow7 <divan.debruin25@gmail.com>
2024-11-25 20:53:26 -0500

    Added laser and meteor image to display surface and got the player movement going

--- spaceshooter/main.py
+++ spaceshooter/main.py
@@ -15,10 +15,25 @@ surf = pygame.Surface((100, 200))
 surf.fill("orange")
 x = 100
 
-# importing an image
+# imports
+#--> import player
 player_surface = pygame.image.load(join("spaceshooter", "images", "player.png")).convert_alpha()
+#--> import star
 star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
+#---> player position
 player_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
+#----> player movement
+player_direction = -1
+#--> import star
+star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
+#--> import meteor
+meteor_surface = pygame.image.load(join("spaceshooter", "images", "meteor.png")).convert_alpha()
+#---> meteor position
+meteor_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
+#--> import laser
+laser_surface = pygame.image.load(join("spaceshooter", "images", "laser.png")).convert_alpha()
+#---> laser position
+laser_rect = laser_surface.get_frect(bottomleft = (20, window_height - 20))
 
 # Generate random star position
 num_stars = 20
@@ -38,9 +53,17 @@ while running:
     for star_position in stars:
         display_surface.blit(star_surface, star_position)
     
+    # meteor
+    display_surface.blit(meteor_surface, (meteor_rect))
+    # laser
+    display_surface.blit(laser_surface, (laser_rect))
     # player
     display_surface.blit(player_surface, (player_rect))
-
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > window_width or player_rect.left < 0:
+        player_direction *= -1
+        
     pygame.display.update()
     
     
