4de799f7fbc290f3933932fefa10831e6086bde0
7IronSnow7 <divan.debruin25@gmail.com>
2024-12-05 14:33:27 -0500

    Messing around with the player speed

--- spaceshooter/main.py
+++ spaceshooter/main.py
@@ -9,7 +9,7 @@ display_surface = pygame.display.set_mode((window_width, window_height))
 # Changed the title of the window
 pygame.display.set_caption("My_first_pygame")
 running = True
-clock = pygame.tine.Clock()
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
@@ -25,7 +25,8 @@ star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).con
 #---> player position
 player_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
 #----> player direction
-player_direction = pygame.math.Vector2(20, -10)
+player_direction = pygame.math.Vector2(1, 0)
+player_speed = 300
 #--> import star
 star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
 #--> import meteor
@@ -44,7 +45,8 @@ num_stars = 20
 stars = [(randint(0, window_width), randint(0, window_height)) for i in range(num_stars)]
 
 while running:
-    clock.tick(10)
+    dt = clock.tick() / 1000
+    print(clock.get_fps())
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,12 +64,10 @@ while running:
     display_surface.blit(meteor_surface, (meteor_rect))
     # laser
     display_surface.blit(laser_surface, (laser_rect))
-    # player
-    display_surface.blit(player_surface, (player_rect))
-    
+ 
     # player movement
-    player_rect.center += player_direction
-    display_surface.blit(player_surface, player_rect.topleft)
+    player_rect.center += player_direction * player_speed * dt
+    display_surface.blit(player_surface, player_rect)
         
     pygame.display.update()
     
