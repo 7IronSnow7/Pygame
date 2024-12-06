8b14f2e67564047c440f0aea3c595ef62f38aaff
7IronSnow7 <divan.debruin25@gmail.com>
2024-12-05 22:07:08 -0500

    Made a lot of changes in terms of readability. Cleaned the code up

--- spaceshooter/main.py
+++ spaceshooter/main.py
@@ -2,6 +2,31 @@ import pygame
 from os.path import join
 from random import randint
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("spaceshooter", "images", "player.png")).convert_alpha()
+        self.rect = self.image.get_frect(center = (window_width / 2, window_height / 2))
+        self.direction = pygame.Vector2()
+        self.speed = 300
+        
+    def update(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT])  - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN])  - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt 
+        
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print("Fire laser")
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
+        self.rect = self.image.get_frect(center = (randint(0, window_width), randint(0, window_height)))
+        
 # General setup
 pygame.init()
 window_width, window_height = 1263, 551
@@ -16,23 +41,24 @@ surf = pygame.Surface((100, 200))
 surf.fill("orange")
 x = 100
 
-
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
+for i in range(20):
+    Star(all_sprites)
 # imports
 #--> import player
-player_surface = pygame.image.load(join("spaceshooter", "images", "player.png")).convert_alpha()
+# player_surface = 
 #--> import star
 star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
 #---> player position
-player_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
-#----> player direction
-player_direction = pygame.math.Vector2(1, 1)
-player_speed = 300
+# player_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
+# player_speed = 300
 #--> import star
-star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
+# star_surface = pygame.image.load(join("spaceshooter", "images", "star.png")).convert_alpha()
 #--> import meteor
 meteor_surface = pygame.image.load(join("spaceshooter", "images", "meteor.png")).convert_alpha()
 #---> meteor position
-meteor_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
+# meteor_rect = player_surface.get_frect(center = (window_width / 2, window_height / 2))
 #--> import laser
 laser_surface = pygame.image.load(join("spaceshooter", "images", "laser.png")).convert_alpha()
 #---> laser position
@@ -41,8 +67,8 @@ laser_rect = laser_surface.get_frect(bottomleft = (20, window_height - 20))
 
 
 # Generate random star position
-num_stars = 20
-stars = [(randint(0, window_width), randint(0, window_height)) for i in range(num_stars)]
+# num_stars = 20
+# stars = [(randint(0, window_width), randint(0, window_height)) for i in range(num_stars)]
 
 while running:
     dt = clock.tick() / 1000
@@ -50,29 +76,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # if event.type == pygame.KEYDOWN:
-        #     print(1)
-        # if event.type == pygame.MOUSEMOTION:
-        #     player_rect.center = event.pos
-        
-    # input
-    print(pygame.mouse.get_pressed())
-    
+
+    all_sprites.update()
+
     # draw game
-    # fill the window with color
     display_surface.fill("black")
+    all_sprites.draw(display_surface)
     
-    # Draw the stars
-    for star_position in stars:
-        display_surface.blit(star_surface, star_position)
-    
-    # meteor
-    display_surface.blit(meteor_surface, (meteor_rect))
-    # laser
-    display_surface.blit(laser_surface, (laser_rect))
-    display_surface.blit(player_surface, player_rect)
-        
     pygame.display.update()
-    
     
 pygame.quit()
