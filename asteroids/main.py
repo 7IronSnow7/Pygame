bc64cb09dc2d67faa6226ebd858205b9483a74d7
7IronSnow7 <divan.debruin25@gmail.com>
2024-12-20 15:49:37 -0500

    Added delta time to help limit the framerate of the asteroids game to 60fps

--- asteroids/main.py
+++ asteroids/main.py
@@ -4,17 +4,21 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+   
     running = True
     
     while running:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
         screen.fill("black")
-        
         pygame.display.flip()
+        
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
         
 pygame.quit()
     
