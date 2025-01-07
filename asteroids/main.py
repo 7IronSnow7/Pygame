d44edde1722d35a746463e299e30265273b40a35
7IronSnow7 <divan.debruin25@gmail.com>
2025-01-07 15:45:48 -0500

    Added the finishing touches to theb asteroid game

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,20 +1,31 @@
+import sys
 import pygame
 from constants import *
 from player import *
-            
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
+    clock = pygame.time.Clock() 
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     
     Player.containers = (updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
-    # player_velocity_x = 200
+    dt = 0
     
     running = True
     
@@ -23,12 +34,18 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 
-        # limit the framerate to 60 fps
-        dt = clock.tick(60) / 1000
-                
-        # update player state
         for obj in updatable:
             obj.update(dt)
+            
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit()
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
         
@@ -36,9 +53,10 @@ def main():
             obj.draw(screen)       
         
         pygame.display.flip()
-       
         
-pygame.quit()
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+        
     
 if __name__ == "__main__":
     main()
