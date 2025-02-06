b46e6bb28d0cbf3499cc03398b4e43863fac70eb
7IronSnow7 <divan.debruin25@gmail.com>
2025-02-06 13:14:42 -0500

    Created the movement for the player paddle

--- pong/sprites.py
+++ pong/sprites.py
@@ -11,10 +11,17 @@ class Player(pygame.sprite.Sprite):
         # rect & movement
         self.rect = self.image.get_frect(center = POS['player'])
         self.direction = 0
+        self.speed = SPEED['player']
         
     def move(self, dt):
         self.rect.centery += self.direction * self.speed * dt
+        self.rect.top = 0 if self.rect.top < 0 else self.rect.top
+        self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
     
     def get_direction(self):
         keys = pygame.key.get_pressed()
         self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        
+    def update(self, dt):
+        self.move(dt)
+        self.get_direction()
