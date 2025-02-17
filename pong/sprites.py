07dfa2466a0a124dabdfca15483c1ff1da016c37
7IronSnow7 <divan.debruin25@gmail.com>
2025-02-16 23:41:28 -0500

    Cleaning up the code

--- pong/sprites.py
+++ pong/sprites.py
@@ -14,23 +14,27 @@ class Paddle(pygame.sprite.Sprite):
         self.old_rect = self.rect.copy()
         self.direction = 0
         self.speed = SPEED['player']
-class Player(Paddle):
-    def __init__(self, groups):
-        super().__init__(groups)
         
     def move(self, dt):
         self.rect.centery += self.direction * self.speed * dt
         self.rect.top = 0 if self.rect.top < 0 else self.rect.top
         self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
     
-    def get_direction(self):
-        keys = pygame.key.get_pressed()
-        self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-        
     def update(self, dt):
         self.old_rect = self.rect.copy()
         self.move(dt)
         self.get_direction()
+    
+class Player(Paddle):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.speed = SPEED['player']
+        
+    
+    def get_direction(self):
+        keys = pygame.key.get_pressed()
+        self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        
         
 class Ball(pygame.sprite.Sprite):
     def __init__(self, groups, paddle_sprites):
